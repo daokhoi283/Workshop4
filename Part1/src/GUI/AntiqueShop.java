6d394fc7bfb5bcbf94037e5fb6307647bef32340
daokhoi283 <91683767+daokhoi283@users.noreply.github.com>
2023-06-09 02:45:05 +0700

    Add files via upload

---
+++ Part1/src/GUI/AntiqueShop.java
@@ -0,0 +1,44 @@
+package GUI;
+import DTO.Item;
+import DTO.Painting;
+import DTO.Statue;
+import DTO.Vase;
+public class AntiqueShop {
+    public static void main(String[] args) {
+        String[] options = {"Create a Vase", "Create a Statue", "Create a Painting", "Display the Item"};
+        Item item = null;
+        int choice = 0;
+        do { 
+            choice = Menu.getChoice(options);
+            switch(choice){
+                case 1:
+                    item = new Vase();
+                    ((Vase)item).inputVase();
+                    break;
+                case 2:
+                    item = new Statue();
+                    ((Statue)item).inputStatue();
+                    break;
+                case 3:
+                    item = new Painting();
+                    ((Painting)item).inputPainting();
+                    break;
+                case 4:
+                    if(item != null){
+                        if(item instanceof Vase){
+                            ((Vase)item).outputVase();
+                        }
+                        else if(item instanceof Statue){
+                            ((Statue)item).outputStatue();
+                        }
+                        else if(item instanceof Painting){
+                            ((Painting)item).outputPainting();
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } while (choice > 0 && choice < 5);
+    }
+}
